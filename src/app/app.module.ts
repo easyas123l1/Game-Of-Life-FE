c968372866a51765cf1bd04ffb7675f3b633486f
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-05-28 19:08:50 -0500

    add all modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { BoardsComponent } from "./components/boards/boards.component";
+import { CreateComponent } from "./components/create/create.component";
+import { PlayComponent } from "./components/play/play.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    BoardsComponent,
+    CreateComponent,
+    PlayComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
