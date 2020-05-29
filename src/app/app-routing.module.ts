a590a030768ff4a8a012d75fd8053d2271dee7d1
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-05-28 19:08:02 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { BoardsComponent } from "./components/boards/boards.component";
+import { PlayComponent } from "./components/play/play.component";
+import { CreateComponent } from "./components/create/create.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "boards", component: BoardsComponent },
+  { path: "create", component: CreateComponent },
+  { path: "play", component: PlayComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
